b38a9cc9609930556174d2614d0c8cd697db71a0
asadovk2 <kamalaa@code.edu.az>
2026-04-19 06:30:33 -0400

    salam

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,30 +1,7 @@
 #!/bin/bash
+tail -n 1000 auth.log | grep "Accepted password" | awk '{print $9}' | sort | uniq | while read user; do
+    if grep -q "Failed password for $user" auth.log; then
+        echo "$user"
+    fi
+done | head -n 1
 
-tail -n 1000 auth.log | awk '
-/Failed password/ {
-    for (i=1; i<=NF; i++) {
-        if ($i == "for") {
-            if ($(i+1) == "invalid") {
-                user=$(i+3)
-            } else {
-                user=$(i+1)
-            }
-            fail[user]++
-        }
-    }
-}
-/Accepted password/ {
-    for (i=1; i<=NF; i++) {
-        if ($i == "for") {
-            user=$(i+1)
-            success[user]++
-        }
-    }
-}
-END {
-    for (u in fail) {
-        if (fail[u] > 1 && success[u] > 0) {
-            print u
-        }
-    }
-}'
